8f6afdff80b929c626ac1e0c155f2325985c67c2
madenjanna-ui <madenjanna@gmail.com>
2026-08-07 17:22:51 +0500

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,1 +1,11 @@
+const bgMusic = new Audio("background.mp3");
+const shuffleSound = new Audio("shuffle.mp3");
+const drawSound = new Audio("draw.mp3");
+const finishSound = new Audio("finish.mp3");
 
+bgMusic.loop = true;
+bgMusic.volume = 0.25;
+
+shuffleSound.volume = 0.7;
+drawSound.volume = 0.7;
+finishSound.volume = 0.7;
